4faf5f6b4c85349eedc55ae60da0bc5445055768
emilijaMarija <z.mari.emilija1@gmail.com>
2025-10-06 21:58:12 +0200

    Fix errors

--- src/scenes/game/level_5.gd
+++ src/scenes/game/level_5.gd
@@ -8,9 +8,12 @@ func _locked_portal_intersect(body: Node2D):
 		return
 	if _door.locked:
 		return
-	exit.emit(Waypoint.WP.END_TUNNEL)
+	call_deferred("_exit_with_end_tunnel")
 
 func _ready() -> void:
 	super._ready()
 	_locked_portal.body_entered.connect(_locked_portal_intersect)
-	
+
+
+func _exit_with_end_tunnel():
+	exit.emit(Waypoint.WP.END_TUNNEL)
