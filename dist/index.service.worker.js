53bbd62c93980349bdf0148d058a51ff15d1c37d
martynasd123 <dargis.martynas@gmail.com>
2025-10-07 00:27:30 +0300

    re-export

--- dist/index.service.worker.js
+++ dist/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1759784601|642022694';
+const CACHE_VERSION = '1759785912|1952753109';
 /** @type {string} */
 const CACHE_PREFIX = 'Collector-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
