5df5e06da550d8f0b09c052541f2da63933c21f9
emilijaMarija <z.mari.emilija1@gmail.com>
2025-10-06 21:34:18 +0200

    Add outro scene

---
+++ scenes/outro_scene.gd
@@ -0,0 +1,41 @@
+extends Node2D
+
+class_name OutroScene
+
+signal end
+
+static var _outro_scene: PackedScene = preload("res://scenes/outro_scene.tscn")
+
+static func create():
+	var outro = _outro_scene.instantiate()
+	return outro
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+enum ANIM {
+	FIRST,
+	SECOND,
+	THIRD,
+	LAST
+}
+
+var animation_names = {
+	ANIM.FIRST: "first_text",
+	ANIM.SECOND: "second text",
+	ANIM.THIRD: "third_text",
+	ANIM.LAST: "end"
+}
+
+var _current_animation = 0
+
+func _input(event: InputEvent) -> void:
+	if (event is InputEventMouseButton or event is InputEventKey) and event.pressed:
+		if not animation_player.current_animation == animation_names[_current_animation]:
+			if not _current_animation == ANIM.LAST:
+				_current_animation += 1
+				animation_player.play(animation_names[_current_animation])
+
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name == animation_names[ANIM.LAST]:
+		end.emit()
--- src/scenes/game/level_6.gd
+++ src/scenes/game/level_6.gd
@@ -1,1 +1,14 @@
 extends Level
+
+var first_enter = true
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if first_enter and body == player:
+		player.frozen = true
+		first_enter = false
+		var outro_scene = OutroScene.create()
+		add_child(outro_scene)
+		outro_scene.end.connect(func():
+			remove_child(outro_scene)
+			player.frozen = false
+		)
